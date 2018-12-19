6507e4769d129f65b804824f80475a19a380209f
Adeonir Kohl <adeonir@gmail.com>
2018-12-19 13:15:15 -0200

    Create initial React Native configuration
    - Create `src` folder
    - Move `App.js` file to `src` and rename to `index.js`
    - Add eslint, editorconfig, reactotron and babel root import

---
+++ src/components/Hello.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Hello = () => <Text>Welcome to React Native!</Text>
+
+export default Hello
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Hello from '~/components/Hello'
+
+const bgColor = '#f5fcff'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: bgColor,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+})
+
+const App = () => (
+  <View style={styles.container}>
+    <Hello style={styles.welcome} />
+  </View>
+)
+
+export default App
